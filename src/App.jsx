d69d79c4450fc72433469f4e8c3bf8ab6ce687ec
NaveenManickDev <naveen.devhub@gmail.com>
2025-06-27 05:16:46 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='font-bold text-3xl text-red-500'>Hi Naveen</h1>
+    </>
+  )
+}
+
+export default App
